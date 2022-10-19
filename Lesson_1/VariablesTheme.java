a649ea73f879b9f2958e4810e725e9de1dc0101d
Rodion <rgsystem@list.ru>
2022-10-19 05:21:50 +0500

    var and if-else

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -1,166 +1,139 @@
-import java.util.HashMap;
-import java.util.Map;
-
-class IfElseStatementTheme{
+class VariablesTheme{
 
     public static void main(String[] args) {
-        System.out.println("1. Перевод псевдокода на язык Java");
-        int age = 29;
-        String sex = "male";
-        double height = 1.90;
-        String name = "Rodion";
+        System.out.println("1. Создание переменных и вывод их значений на консоль\n");
+        byte yearsOfUse = 8;
+        short ram = 8192;
+        byte sysBitDepth = 64;
+        long corAmo = 2L;
+        float corFreq = 2.4F;
+        double dispRefRate = 60.0;
+        char logo = 'F';
+        boolean isHaveRemont = true;
+        System.out.println("Yeats of use - " + yearsOfUse + " years");
+        System.out.println("RAM - " + ram + " mb");
+        System.out.println("System bit depth - " + sysBitDepth + " bit");
+        System.out.println("Cores amount - " + corAmo + " psc");
+        System.out.println("Frequency of core - " + corFreq + " GHz");
+        System.out.println("Display refresh rate - " + dispRefRate + " Hz");
+        System.out.println("Logotype - " + logo);
+        System.out.println("Is have remont - " + isHaveRemont + "\n");
 
-        if (age > 20) {
-            System.out.println("Age more than 20");
-        } else {
-            System.out.println("Age less then 20");
-        }
+        System.out.println("2. Расчет стоимости товара со скидкой\n");
+        double penPrice = 100.0;
+        double bookPrice = 200.0;
+        double disProc = 11;
+        double disSum = (penPrice + bookPrice) * disProc / 100.0;
+        double sumWithDis = (penPrice + bookPrice) - disSum;
+        System.out.println("Сумма скидки - " + disSum);
+        System.out.println("Общая стоимость товаров со скидкой - " + sumWithDis + "\n");
 
-        if (!sex.equals("male")) {
-            System.out.println("female");
-        } else {
-            System.out.println("male");
-        }
+        System.out.println("3. Вывод на консоль слова JAVA\n");
+        System.out.println("   J    a  v     v  a       \n   J   a a  v   v  a a      ");
+        System.out.println("J  J  aaaaa  V V  aaaaa          \n JJ  a     a  V  a     a\n");
 
-        if (height < 1.80) {
-            System.out.println("Height less then 1.80");
-        } else {
-            System.out.println("Height more then 1.80");
-        }
+        System.out.println("4. Отображение min и max значений числовых типов данных\n");
+        byte byteMaxVal = 127;
+        short shortMaxVal = 32767;
+        int intMaxVal = 2147483647;
+        long longMaxVal = 9223372036854775807L;
+        System.out.println("Byte max val = " + byteMaxVal);
+        byteMaxVal++;
+        System.out.println("Byte max val after inc = " + byteMaxVal);
+        byteMaxVal--;
+        System.out.println("Byte max val after dec = " + byteMaxVal + "\n");
+        System.out.println("Short max val = " + shortMaxVal);
+        shortMaxVal++;
+        System.out.println("Short max val after inc = " + shortMaxVal);
+        shortMaxVal--;;
+        System.out.println("Short max val after dec = " + shortMaxVal + "\n");
+        System.out.println("Int max val = " + intMaxVal);
+        intMaxVal++;
+        System.out.println("Int max val after inc = " + intMaxVal);
+        intMaxVal--;
+        System.out.println("Int max val after dec = " + intMaxVal + "\n");
+        System.out.println("Long max val = " + longMaxVal);
+        longMaxVal++;
+        System.out.println("Long max val after inc = " + longMaxVal);
+        longMaxVal--;
+        System.out.println("Long max val after dec = " + longMaxVal + "\n");
 
-        char firstLetterOfName = name.charAt(0);
-        if (firstLetterOfName == 'M') {
-            System.out.println("First letter of name - M");
-        } else if (firstLetterOfName == 'I') {
-            System.out.println("First letter of name - I");
-        } else {
-            System.out.println("First letter of name - " + Character.toString(firstLetterOfName));
-        }
-        System.out.println();
+        System.out.println("5. Перестановка значений переменных\n");
+        int a = 2;
+        int b = 5;
+        System.out.println("Через третью переменную\nНачальные значения a = " + a + " b = " + b);
+        int tmp = a;
+        a = b;
+        b = tmp;
+        System.out.println("Новые значения a = " + a + " b = " + b + "\n");
+        System.out.println("С помощью арифметических операций\nНачальные значения a = " + a + " b = " + b);
+        a += b;
+        b = a - b;
+        a -= b;
+        System.out.println("Новые значения a = " + a + " b = " + b + "\n");
+        System.out.println("С помощью побитовых операций\nНачальные значения a = " + a + " b = " + b);
+        a = a ^ b;
+        b = b ^ a;
+        a = a ^ b;
+        System.out.println("Новые значения a = " + a + " b = " + b + "\n");
 
-        System.out.println("2. Поиск max и min числа");
-        int a = 5;
-        int b = 3;
-        System.out.println("a = " + a + " b = " + b);
-        if (a > b) {
-            System.out.println("a max, b min\n");
-        } else if (b > a) {
-            System.out.println("b max, a min\n");
-        } else {
-            System.out.println("a equals to b\n");
-        }
+        System.out.println("6. Вывод символов и их кодов\n");
+        char hash = '#';
+        char amp = '&';
+        char dog = '@';
+        char pow = '^';
+        char unSpc = '_';
+        System.out.println("Код символа - " + (int) hash + " символ - " + hash);
+        System.out.println("Код символа - " + (int) amp + " символ - " + amp);
+        System.out.println("Код символа - " + (int) dog + " символ - " + dog);
+        System.out.println("Код символа - " + (int) pow + " символ - " + pow);
+        System.out.println("Код символа - " + (int) unSpc + " символ - " + unSpc + "\n");
 
-        System.out.println("3. Работа с числом");
-        boolean isZero = a == 0;
-        boolean isEvenNum = false;
-        boolean isPositive = false;
-        if (!isZero) {
-            if (a % 2 == 0) {
-                isEvenNum = true;
-            }
-            if (a > 0) {
-                isPositive = true;
-            }
-            System.out.println("Origin number = " + a);
-            System.out.println("Is even number - " + isEvenNum);
-            System.out.println("Is positive num - " + isPositive + "\n");
-        }
+        System.out.println("7. Отображение количества сотен, десятков и единиц числа\n");
+        short num = 123;
+        System.out.println("Число " + num + " содержит");
+        System.out.println((num / 100 % 10) + " сотен");
+        System.out.println(num / 10 % 10 + " десятков");
+        System.out.println(num % 10 + " единиц\n");
 
-        System.out.println("4. Поиск одинаковых цифр в числах");
-        a = 123;
-        b = 323;
-        int hundredsOfA = (a / 100) % 10;
-        int dozensOfA = (a / 10) % 10;
-        int unitsOfA = a % 10;
-        int hundredsOfB = (b / 100) % 10;
-        int dozensOfB = (b / 10) % 10;
-        int unitsOfB = b % 10;
-        System.out.println("Origin numbers a = " + a + ", b = " + b);
-        if (unitsOfA == unitsOfB) {
-            System.out.println("Одинаковая цифра " + unitsOfA + ", разряд 1\n");
-        } else if (dozensOfA == dozensOfB) {
-            System.out.println("Одинаковая цифра " + dozensOfA + ", разряд 2\n");
-        } else if (hundredsOfA == hundredsOfB) {
-            System.out.println("Одинаковое число " + hundredsOfA + ", разряд 3\n");
-        } else {
-            System.out.println("Одинаковых цифр стоящих в одном разряде нет\n");
-        }
+        System.out.println("8. Вывод на консоль ASCII-арт Дюка\n");
+        char slash = '/';
+        char backSlash = '\\';
+        char lScope = '(';
+        char rScope = ')';
+        unSpc = '_';
+        char spc = ' ';
+        System.out.println(Character.toString(spc) + Character.toString(spc) + Character.toString(spc) +
+                Character.toString(spc) + Character.toString(slash) + Character.toString(backSlash));
+        System.out.println(Character.toString(spc) + Character.toString(spc) + Character.toString(spc) +
+                Character.toString(slash) + Character.toString(spc) + Character.toString(spc) + Character.toString(backSlash));
+        System.out.println(Character.toString(spc) + Character.toString(spc) + Character.toString(slash) +
+                Character.toString(unSpc) + Character.toString(lScope) + Character.toString(spc) + Character.toString(rScope) +
+                Character.toString(backSlash));
+        System.out.println(Character.toString(spc) + Character.toString(slash) + Character.toString(spc) +
+                Character.toString(spc) + Character.toString(spc) + Character.toString(spc) + Character.toString(spc) +
+                Character.toString(spc) + Character.toString(backSlash));
+        System.out.println(Character.toString(slash) + Character.toString(unSpc) + Character.toString(unSpc) +
+                Character.toString(unSpc) + Character.toString(unSpc) + Character.toString(slash) + Character.toString(backSlash) +
+                Character.toString(unSpc) + Character.toString(unSpc) + Character.toString(backSlash));
 
-        System.out.println("5. Определение буквы, числа или символа по их коду");
-        // при проверке используйте символы, а не их коды
-        boolean isUpperCaseLetter = false;
-        boolean isLowerCaseLetter = false;
-        boolean isDigit = false;
-        char someSymbol = '\u0057';
-        // A-Z = 65-90; a-z = 97-122; 0-9 = 48-57
-        if (someSymbol >= 'A' && someSymbol <= 'Z') {
-            isUpperCaseLetter = true;
-        }
-        if (someSymbol >= 'a' && someSymbol <= 'z') {
-            isLowerCaseLetter = true;
-        }
-        if (someSymbol >= '0' && someSymbol <= '9') {
-            isDigit = true;
-        }
-        System.out.println("Символ - " + someSymbol); // 87 код
-        System.out.println("Большая буква - " + isUpperCaseLetter);
-        System.out.println("Маленькая буква - " + isLowerCaseLetter);
-        System.out.println("Число - " + isDigit);
-        if (!isUpperCaseLetter && !isLowerCaseLetter && !isDigit) {
-            System.out.println("Не буква и не число");
-        }
-        System.out.println();
+        System.out.println("9. Произведение и сумма цифр числа\n");
+        num = 345;
+        System.out.println("Дано число " + num);
+        int hundrers = num / 100 % 10;
+        int dozens = num / 10 % 10;
+        int units = num % 10;
+        System.out.println("Сумма цифр числа " + num + " = " + (hundrers + dozens + units));
+        System.out.println("Произведение цифр числа " + num + " = " + hundrers * dozens * units + "\n");
 
-        System.out.println("6. Определение суммы вклада и начисленных банком %");
-        double depositSum = 300000;
-        double prcPerAnnum = 0;
-        if (depositSum < 100000) {
-            prcPerAnnum = depositSum * 0.05;
-        } else if (depositSum >= 100000 && depositSum <= 300000) {
-            prcPerAnnum = depositSum * 0.07;
-        } else if (depositSum > 300000) {
-            prcPerAnnum = depositSum * 0.10;
-        }
-        System.out.println("Сумма вклада - " + depositSum);
-        System.out.println("Начисленный % - " + prcPerAnnum); // НЕПРАВИЛЬНО
-        System.out.println("Итоговая сумма с % - " + (depositSum + prcPerAnnum) + "\n");
-
-        System.out.println("7. Определение оценки по предметам");
-        Map<String, Integer> grades = new HashMap<>();
-        grades.put("History", 59);
-        grades.put("Programming", 91);
-        for (String s : grades.keySet()) {
-            int prc = grades.get(s);
-            int gr = 0;
-            if (prc <= 60) {
-                gr = 2;
-            } else if ((prc > 60) && (prc <= 73)) {
-                gr = 3;
-            } else if ((prc > 73) && (prc <= 91)) {
-                gr = 4;
-            } else if (prc > 91) {
-                gr = 5;
-            }
-            System.out.println(s + " " + gr);
-        }
-        System.out.println();
-
-        System.out.println("8. Расчет прибыли");
-        double rantPerMon = 5000;
-        double salesPerMon = 13000;
-        double costPerMon = 9000;
-        double profitPerMon = salesPerMon - costPerMon - rantPerMon;
-        double annualProfit = profitPerMon * 12;
-        if (annualProfit > 0) {
-            System.out.println("+" + annualProfit + "\n");
-        } else {
-            System.out.println(annualProfit + "\n");
-        }
-
-        System.out.println("9. Подсчет количества банкнот");
-        int sum = 567;
-        System.out.println((sum / 100) % 10 + " по сотне");
-        System.out.println((sum / 10) % 10 + " по десять");
-        System.out.println(sum % 10+ " по одному");
+        System.out.println("10. Преобразование секунд\n");
+        int secAmount = 86399;
+        int intHours = secAmount / 3600;
+        int intHoursInSec = intHours * 3600;
+        int secAmountWithoutHours = secAmount - intHoursInSec;
+        int intMinutes = secAmountWithoutHours / 60;
+        int intMinutesInSec = intMinutes * 60;
+        int secWithoutHoursAndMinutes = (secAmount - intHoursInSec) - intMinutesInSec;
+        System.out.println("" + intHours + ":" + intMinutes + ":" + secWithoutHoursAndMinutes);
     }
 }
