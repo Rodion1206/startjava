0573cad1c615da3549a89797a51c2105cef0931b
Rodion <rgsystem@list.ru>
2022-10-29 19:58:03 +0500

    lesson 2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+class Person {
+
+	String gender = "male";
+	String name = "Alex";
+	double height = 1.80;
+	double weight = 80;
+	int age = 28;
+
+	void go() {
+		System.out.println("Идет");
+	}
+
+	void sit() {
+		System.out.println("Сидит");
+	}
+
+	void run() {
+		System.out.println("Бежит");	
+	}
+
+	void speak() {
+		System.out.println("Говорит");
+	}
+
+	void learnJava() {
+		System.out.println("Учит джава");
+	}
+}
