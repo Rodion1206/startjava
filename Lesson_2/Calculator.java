feb433488efb13152cb629b92e0880ad4ce63254
Rodion <rgsystem@list.ru>
2022-11-09 21:07:52 +0500

    qwe

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -2,31 +2,32 @@ class Calculator {
 
     //  +, -, *, /, ^, %
     public void calculate(char operator, double a, double b) {
+        double result = 0;
         switch (operator) {
             case '+' :
-                System.out.println(a + b);
+                result = a + b;
                 break;
             case '-' :
-                System.out.println(a - b);
+                result = a - b;
                 break;
             case '*' :
-                System.out.println(a * b);
+                result = a * b;
                 break;
             case '/' :
-                System.out.println(a / b);
+                result = a / b;
                 break;
             case '^' :
                 double result = 1;
                 for (int i = 1; i <= b; i++) {
                     result *= a;
                 }
-                System.out.println(result);
                 break;
             case '%' :
-                System.out.println(a % b);
+                result = a % b;
                 break;
             default:
                 System.out.println("Некорректный оператор");
         }
+        System.out.println(result);
     }
 }
