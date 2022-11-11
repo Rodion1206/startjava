d3236d324acd1c23027fd42a9ef7303c829506b8
Rodion <rgsystem@list.ru>
2022-11-11 21:00:42 +0500

    new structure

--- src/com/startjava/lesson_2_3/guess/Player.java
+++ src/com/startjava/lesson_2_3/guess/Player.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_2_3.guess;
+
 // Player — описывает игроков
 public class Player {
 
--- src/com/startjava/lesson_2_3/person/Person.java
+++ src/com/startjava/lesson_2_3/person/Person.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_2_3.person;
+
 class Person {
 
 	String gender = "male";
